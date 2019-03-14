a45f04ccdfa64311f2013948003311cc4fe33439
faucoant <antoine.fauconnier@yahoo.fr>
2019-03-14 11:29:17 +0100

    accueil bien

--- Views/php/accueil.php
+++ Views/php/accueil.php
@@ -1,60 +1,58 @@
 
 <html>
-    <head>
-        <meta charset="utf_8" />
-        <title>weCon</title>
-        <link rel="stylesheet" type="text/css" href="Views/css/style.css">
-        <link rel="stylesheet" type="text/css" href="../css/style.css">
-    </head>
+<head>
+    <meta charset="utf_8" />
+    <title>weCon</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-    <body>
-    <div class="background">
-        <nav id="header">
-            <div class="elementHeader">
-                <ul class="header">
-                    <li class="element hide-line icon"><a href="index.php?action=Accueil">
-                            <img src="Model/LgoBlueBg.PNG" style="width: 50%;"></a></li>
-                    <li class="element"><a href="index.php?action=Accueil">Accueil</a></li>
-                    <li class="element"><a href="boucles.php">Produits</a></li>
-                    <li class="element"><a href="APropos.html">À propos</a></li>
-                    <li class="element"><a href="index.php?action=Send_Message">Contact</a></li>
-                    <li class="element"><a href="index.php?action=Connexion">Connexion</a></li>
-                </ul>
-            </div>
-        </nav>
-        <h1>Nous vous connectons à votre maison</h1>
-        <button class="en-savoir-plus">En savoir plus</button>
+<body>
+<div class="background">
+    <nav id="header">
+        <img class="image" href="accueil.php" src="ressources/weCon.png">
+        <div class="elementHeader">
+            <ul class="header">
+                <li class="element"><a href="accueil.php">Accueil</a></li>
+                <li class="element"><a href="boucles.php">Produits</a></li>
+                <li class="element"><a href="APropos.html">À propos</a></li>
+                <li class="element"><a href="test.php">Contact</a></li>
+                <li class="element"><a href="test.php">Connexion</a></li>
+            </ul>
+        </div>
+    </nav>
+    <h1>Nous vous connectons à votre maison</h1>
+    <button class="en-savoir-plus">En savoir plus</button>
+</div>
+
+<div class="gauche">
+    <img class="pic" src="ressources/first.jpg" alt="" width="360" height="260" />
+    <div class="textl">
+        <p align="justify"><strong>Un accès à vos données simplifié et fluide</strong>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
     </div>
-
-    <div class="gauche">
-        <img class="pic" src="Model/ressources/first.jpg" alt="" width="360" height="260" />
-        <div class="textl">
-            <p align="justify"><strong>Un accès à vos données simplifié et fluide</strong>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </p>
-        </div>
-        <img class="pic" src="Model/ressources/third.jpg" alt="" width="360" height="260" />
-        <div class="textl">
-            <p align="justify"><strong>Des techniciens à votre service</strong>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
-        </div>
+    <img class="pic" src="ressources/third.jpg" alt="" width="360" height="260" />
+    <div class="textl">
+        <p align="justify"><strong>Des techniciens à votre service</strong>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
     </div>
+</div>
 
-    <div class="droite">
-        <div class="textr">
-            <p align="justify"><strong>Une connexion sécurisée à votre maison à n'importe quel moment</strong>
-                <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.</p>
-        </div>
-        <img class="pic" src="Model/ressources/second.jpg" alt="" width="360" height="260" />
-        <div class="textr">
-                <p align="justify"><strong>Une consommation d’électricité réduite grâce à
-                        une gestion responsable de vos ressources</strong>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
-        </div>
-        <img class="pic" src="Model/ressources/fourth.jpg" alt="" width="360" height="260" />
+<div class="droite">
+    <div class="textr">
+        <p align="justify"><strong>Une connexion sécurisée à votre maison à n'importe quel moment</strong>
+            <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.</p>
     </div>
+    <img class="pic" src="ressources/second.jpg" alt="" width="360" height="260" />
+    <div class="textr">
+        <p align="justify"><strong>Une consommation d’électricité réduite grâce à
+                une gestion responsable de vos ressources</strong>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
+    </div>
+    <img class="pic" src="ressources/fourth.jpg" alt="" width="360" height="260" />
+</div>
 
-    </body>
+</body>
 
 <footer>
 
