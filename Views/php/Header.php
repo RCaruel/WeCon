209973388815bb427cc1ed3ca8d7ca60e3f43581
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2019-03-14 11:30:15 +0100

    Add files via upload

---
+++ Views/php/Header.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="style.css">
+
+<nav id="header">
+    <div class="elementHeader">
+        <ul class="header">
+            <li class="element hide-line icon"><a href="APropos.html"> <img src="Ressources/LogoBlanc.png" width="130"/></a> </li>
+            <li class="element"><a href="tableaux.php">Accueil</a></li>
+            <li class="element"><a href="boucles.php">Produits</a></li>
+            <li class="element active"><a href="APropos.html">À propos</a></li>
+            <li class="element"><a href="test.php">Contact</a></li>
+            <li class="element"><a href="test.php">Connexion</a></li>
+        </ul>
+    </div>
+</nav>
