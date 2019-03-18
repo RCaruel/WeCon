a85403f4514e3e610c32c025044a06de361963f8
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2019-03-18 17:25:08 +0100

    Update Header.php

--- Views/php/Header.php
+++ Views/php/Header.php
@@ -1,14 +1,20 @@
-<link rel="stylesheet" href="style.css">
-
-<nav id="header">
-    <div class="elementHeader">
-        <ul class="header">
-            <li class="element hide-line icon"><a href="APropos.html"> <img src="Ressources/LogoBlanc.png" width="130"/></a> </li>
-            <li class="element"><a href="tableaux.php">Accueil</a></li>
-            <li class="element"><a href="boucles.php">Produits</a></li>
-            <li class="element active"><a href="APropos.html">À propos</a></li>
-            <li class="element"><a href="test.php">Contact</a></li>
-            <li class="element"><a href="test.php">Connexion</a></li>
-        </ul>
-    </div>
+<link rel="stylesheet" href="header.css">
+<html>
+<body>
+<header>
+    <ul>
+        <li class="element hide-line icon"><a href="APropos.html"> <img src="Ressources/LogoBlanc.png" width="130"/></a> </li>
+        <li class="element"><a href="tableaux.php">Accueil</a></li>
+        <li class="element"><a href="boucles.php">Produits</a></li>
+        <li class="element active"><a href="APropos.html">À propos</a></li>
+        <li class="element"><a href="test.php">Contact</a></li>
+        <li class="element"><a href="test.php">Connexion</a></li>
+    </ul>
+</header>
 </nav>
+<script type="text/javascript">
+    $(".header").fitText();
+
+</script>
+</body>
+</html>
