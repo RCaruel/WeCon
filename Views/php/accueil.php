e80b9a4dd0ec4f8a25314da9c8125f796b0a7b19
RCaruel <RCaruel@users.noreply.github.com>
2019-03-19 09:25:26 +0100

    modif accueil.php + style.css
    modif pour ajouter le footer

--- Views/php/accueil.php
+++ Views/php/accueil.php
@@ -7,49 +7,51 @@
 </head>
 
 <body>
-<div class="background">
-    <nav id="header">
-        <a href="index.php?action=Accueil"><img class="image" href="accueil.php" src="Model/ressources/weCon.png"></a>
-        <div class="elementHeader">
-            <ul class="header">
-                <li class="element"><a href="index.php?action=Accueil">Accueil</a></li>
-                <li class="element"><a href="boucles.php">Produits</a></li>
-                <li class="element"><a href="APropos.html">À propos</a></li>
-                <li class="element"><a href="index.php?action=Send_Message">Contact</a></li>
-                <li class="element"><a href="index.php?action=Connexion">Connexion</a></li>
-            </ul>
+<div class = "content">
+    <div class="background">
+        <nav id="header">
+            <a href="index.php?action=Accueil"><img class="image" href="accueil.php" src="Model/ressources/weCon.png"></a>
+            <div class="elementHeader">
+                <ul class="header">
+                    <li class="element"><a href="index.php?action=Accueil">Accueil</a></li>
+                    <li class="element"><a href="boucles.php">Produits</a></li>
+                    <li class="element"><a href="APropos.html">À propos</a></li>
+                    <li class="element"><a href="index.php?action=Send_Message">Contact</a></li>
+                    <li class="element"><a href="index.php?action=Connexion">Connexion</a></li>
+                </ul>
+            </div>
+        </nav>
+        <h1>Nous vous connectons à votre maison</h1>
+        <button class="en-savoir-plus">En savoir plus</button>
+    </div>
+
+    <div class="gauche">
+        <img class="pic" src="Model/ressources/first.jpg" alt="" width="380" height="280" />
+        <div class="textl">
+            <p align="justify"><strong>Un accès à vos données simplifié et fluide.Parler du réseau télécom</strong></p>
         </div>
-    </nav>
-    <h1>Nous vous connectons à votre maison</h1>
-    <button class="en-savoir-plus">En savoir plus</button>
-</div>
+        <img class="pic" src="Model/ressources/third.jpg" alt="" width="380" height="280" />
+        <div class="textl">
+            <p align="justify"><strong>Des techniciens à votre service</strong>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
+        </div>
+    </div>
 
-<div class="gauche">
-    <img class="pic" src="Model/ressources/first.jpg" alt="" width="380" height="280" />
-    <div class="textl">
-        <p align="justify"><strong>Un accès à vos données simplifié et fluide.Parler du réseau télécom</strong></p>
+    <div class="droite">
+        <div class="textr">
+            <p align="justify"><strong>Une connexion sécurisée à votre maison à n'importe quel moment o&ugrave; vous le souhaitez. Gr&acirc;ce &agrave; notre site internet et votre espace de connexion vous ne perdez pas vos données. Tout est mis en place pour que vous n'ayez aucune difficulté &agrave; avoir acc&egrave;s &agrave; celle-ci gr&acirc;ce &agrave; notre version mobile.</strong>
+            </p>
+        </div>
+        <img class="pic" src="Model/ressources/second.jpg" alt="" width="380" height="280" />
+        <div class="textr">
+            <p align="justify"><strong>Une consommation d’électricité réduite grâce à
+                    une gestion responsable de vos ressources</strong>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
+        </div>
+        <img class="pic" src="Model/ressources/fourth.jpg" alt="" width="380" height="280" />
     </div>
-    <img class="pic" src="Model/ressources/third.jpg" alt="" width="380" height="280" />
-    <div class="textl">
-        <p align="justify"><strong>Des techniciens à votre service</strong>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
-    </div>
-</div>
 
-<div class="droite">
-    <div class="textr">
-        <p align="justify"><strong>Une connexion sécurisée à votre maison à n'importe quel moment o&ugrave; vous le souhaitez. Gr&acirc;ce &agrave; notre site internet et votre espace de connexion vous ne perdez pas vos données. Tout est mis en place pour que vous n'ayez aucune difficulté &agrave; avoir acc&egrave;s &agrave; celle-ci gr&acirc;ce &agrave; notre version mobile.</strong>
-        </p>
-    </div>
-    <img class="pic" src="Model/ressources/second.jpg" alt="" width="380" height="280" />
-    <div class="textr">
-        <p align="justify"><strong>Une consommation d’électricité réduite grâce à
-                une gestion responsable de vos ressources</strong>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
-    </div>
-    <img class="pic" src="Model/ressources/fourth.jpg" alt="" width="380" height="280" />
 </div>
-
 <?php include "Views/html/footer.html"; ?>
 
 </body>
