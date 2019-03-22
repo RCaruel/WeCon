e9a828029c301aea4752cd42a689ebf16ecd1418
St0rmSnake <St0rmSnake@users.noreply.github.com>
2019-03-22 15:07:00 +0100

    niveau header (pas fini)

--- Views/php/Header.php
+++ Views/php/Header.php
@@ -1,20 +1,29 @@
-<link rel="stylesheet" href="header.css">
+<link rel="stylesheet" href="Views/css/styleheader.css">
+<link rel="stylesheet" href="../css/styleheader.css">
 <html>
 <body>
 <header>
-    <ul>
-        <li class="element hide-line icon"><a href="APropos.html"> <img src="Ressources/LogoBlanc.png" width="130"/></a> </li>
-        <li class="element"><a href="tableaux.php">Accueil</a></li>
-        <li class="element"><a href="boucles.php">Produits</a></li>
-        <li class="element active"><a href="APropos.html">À propos</a></li>
-        <li class="element"><a href="test.php">Contact</a></li>
-        <li class="element"><a href="test.php">Connexion</a></li>
-    </ul>
-</header>
+          <span class="titre"><a href=""><img src="/WeCon/Model/LogoBlueBg.PNG" width="130"/></a></span>
+        <ul>
+            <li class="element"><a href="tableaux.php">Accueil</a></li>
+            <li class="element"><a href="boucles.php">Produits</a></li>
+            <li class="element active"><a href="APropos.html">À propos</a></li>
+            <li class="element"><a href="test.php">Contact</a></li>
+            <li class="element connect"><a href="test.php">Connexion</a>
+           		<ul class="submenu">
+					<li class="co"><a href="test.php">Espace Client</a></li>
+					<li class="co"><a href="test.php">Espace Entreprise</a></li>
+					<li class="co"><a href="test.php">Espace Technicien</a></li>
+            	</ul>
+        	</li>
+        </ul>
+
+    </header>
+
 </nav>
 <script type="text/javascript">
-    $(".header").fitText();
-
-</script>
+        $(".header").fitText();
+        
+    </script>
 </body>
 </html>
