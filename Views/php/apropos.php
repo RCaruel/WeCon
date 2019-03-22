215ee464f192ccd34ade19f72daa1db2af0c0187
jeanrannou <47852624+jeanrannou@users.noreply.github.com>
2019-03-22 14:25:56 +0100

    Create apropos.php

---
+++ Views/php/apropos.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>À propos</title>
+</head>
+<body>
+
+<?php include("Header.php"); ?>
+
+
+<div class>
+    <img id="Accueil" class = "image" src="Ressources/georgie-cobbs-459520-unsplash.jpg" alt="Image d'accueil" />
+    <p class = "slogan"> La maison de demain, maintenant</p>
+    <div id = logo>
+        <img class = "logo" src = "Ressources/LogoBleu.png" alt="Logo" />
+    </div>
+
+</div>
+
+
+<div id="intro">
+    <p ALIGN="center" class = "NotreVision">Notre vision</p>
+    <img class = "Vision" src="Ressources/rawpixel-191102-unsplash.jpg" />
+    <p ALIGN="left" class = "premierparagraphe">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut </p>
+</div>
+
+<div id ="deuxieme">
+    <p ALIGN="center" class = "NotreMission">Notre mission</p>
+    <p ALIGN="center" class = "deuxiemeparagraphe">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </p>
+    <img class = "Icones" src="Ressources/Icones.jpg"
+</div>
+
+
+<div id = "troisieme">
+    <p ALIGN="center" class = "NosValeurs">Nos valeurs</p>
+    <p ALIGN="left" class = "troisiemeparagraphe">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut </p>
+    <img class = "Valeurs" src="Ressources/rawpixel-597440-unsplash.jpg" />
+</div>
+
+<div id = "quatrieme">
+    <p ALIGN="center" class = "NotreEquipe">Notre équipe</p>
+    <a href="https://www.linkedin.com/in/gabriel-radigu%C3%A9-1013a3156/"target="_blank">
+        <img class = "Gabriel" src="Ressources/Gabriel.jpg"/>
+    </a>
+    <a href="https://www.linkedin.com/in/antoine-fauconnier-a11425150/"target="_blank">
+        <img class = "Antoine" src="Ressources/Antoine.jpg"/>
+    </a>
+    <a href="https://www.linkedin.com/in/r%C3%A9mi-caruel-05408517b/"target="_blank">
+        <img class = "Remi" src="Ressources/Rémi.jpg"/>
+    </a>
+
+        <img class = "Robin" src="Ressources/Robin.jpg"/>
+
+    <a href="https://www.linkedin.com/in/hicham-tamarin-87b863170/"target="_blank">
+        <img class = "Hicham" src="Ressources/Hicham.jpg"/>
+    </a>
+    <a href="https://www.linkedin.com/in/jean-rannou-420a3816a/"target="_blank">
+        <img class = "Jean" src="Ressources/Jean.jpg"/>
+    </a>
+
+</div>
+
+
+</body>
+
+
+
+
+
+</html>
+
+
