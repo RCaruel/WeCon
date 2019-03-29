8c5e566a4a54e7eeef976d318e9c0db81330b064
St0rmSnake <45652209+St0rmSnake@users.noreply.github.com>
2019-03-29 12:44:41 +0100

    Update Header.php

--- Views/php/Header.php
+++ Views/php/Header.php
@@ -2,22 +2,24 @@
 <link rel="stylesheet" href="../css/styleheader.css">
 <html>
 <body>
+<html>
+<body>
 <header>
-          <span class="titre"><a href=""><img src="/WeCon/Model/LogoBlueBg.PNG" width="130"/></a></span>
+          <span class="titre"><a href=""><img src="Logo.PNG" width="130"/></a></span>
         <ul>
-            <li class="element"><a href="index.php?action=Accueil">Accueil</a></li>
-            <li class="element"><a href="index.php?action=Produit">Produits</a></li>
-            <li class="element active"><a href="index.php?action=Apropos">À propos</a></li>
-            <li class="element"><a href="index.php?action=Contact">Contact</a></li>
-            <li class="element"><a href="index.php?action=Connexiontech">Connexion</a></li>
+            <li class="element"><a href="tableaux.php">Accueil</a></li>
+            <li class="element"><a href="boucles.php">Produits</a></li>
+            <li class="element active"><a href="APropos.html">À propos</a></li>
+            <li class="element"><a href="test.php">Contact</a></li>
+            <li class="connect"><a href="#">Connexion </a>
+                <ul class="submenu">
+                    <li><a href="test.php">Espace Client</a></li>
+                    <li><a href="test.php">Espace Entreprise</a></li>
+                    <li><a href="test.php">Espace Technicien</a></li>
+                </ul>
+            </li>
         </ul>
 
     </header>
-
-</nav>
-<script type="text/javascript">
-        $(".header").fitText();
-        
-    </script>
 </body>
 </html>
