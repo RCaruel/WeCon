8ca69c84f0ee5952a559abed06ecf0cfe5d2c5c7
faucoant <antoine.fauconnier@yahoo.fr>
2019-04-02 08:38:14 +0100

    + espace

---
+++ Views/html/EspaceClient.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>WeCon</title>
+    <link rel="stylesheet"  href="../css/EspaceClient.css">
+    <link rel="stylesheet"  href="Views/css/EspaceClient.css">
+</head>
+
+<body>
+
+<nav>
+    <img src="Ressources/logoBluebg.PNG">
+    <div class = "anc"><a href="#Tableaudebord"><button id = "StatsGen">Tableau de bord</button></a></div>
+    <div class = "line"></div>
+    <div class = "anc"><a href="#Gestiondescapteurs"><button id = "GestCapt">Gestion des capteurs</button></a></div>
+    <div class = "line"></div>
+    <div class = "anc"><a href="#Gestiondescomptes"><button id = "GestComptes">Gestion des comptes</button></a></div>
+    <div class = "line"></div>
+    <div class = "anc"><a href="#Parametre"><button id = "Parametre">Param&egrave;tre</button></a></div>
+    <div class = "line"></div>
+
+</nav>
+
+
+</body>
+</html>
