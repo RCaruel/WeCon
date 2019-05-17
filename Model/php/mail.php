2919306421e52a3e83933c2aa2b1fc6d1bcef4f7
RCaruel <RCaruel@users.noreply.github.com>
2019-05-17 15:30:37 +0200

    Ajout Fonction Envoi mail page Contacter

--- Model/php/mail.php
+++ Model/php/mail.php
@@ -34,4 +34,43 @@ function SendMailInscription($name, $mail, $motdepasse){
     mail($mail,$sujet,$message, $header);
 
 }
+
+function SendMailRequest($nom, $prenom, $mail, $message){
+
+//=====Définition du sujet.
+    $sujet = "Vous nous avez contacté Monsieur ". $nom.".";
+//=========
+
+//=====Création du header de l'e-mail.
+    $header = "MIME-Version: 1.0\r\n";
+    $header.= "From: WeCon.com<remi.caruel@gmail.com>"."\n";
+    $header.= "Content-Type:text/html; charset = 'utf-8'"."\n";
+    $header.= "Content-Transfert-Encoding : 8bit";
+
+    $message = '
+    <html>
+        <body>
+            <div align="center" style="text-decoration: underline;">
+                <h3>Bonjour monsieur ' .$prenom . " " . $nom."</h3>
+            </div>
+            <p align='justify'>Nous avons bien reçu votre message et nous vous recontacterons dès que possible.
+                <br>
+                Voici vos informations :
+                Nom : " . $nom . "
+                Prenom : " . $prenom . "
+                Mail : " . $mail . "
+                Message : " .$message. "
+                <br>
+                Merci de ne pas renvoyer de mails et d'attendre notre retour afin de ne pas ralentir le traitement de votre demande.
+            <br><br>
+            </p>
+            <p>Cordialement<br>WeCon</p>
+            <h6><br><br><br>Nous vous rappelons que ce mail a été envoyé automatiquement, si vous n'êtes pas à l'origine de ce message,
+            merci de contacter le support sur le site WeCon.com</h6>
+        </body>
+    </html>
+    ";
+
+    mail($mail,$sujet,$message, $header);
+}
 ?>
