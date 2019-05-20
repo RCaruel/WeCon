c5de812c57a0812cff6543c9b2b072c7cd3b0339
St0rmsnake <dofen59@gmail.com>
2019-05-21 01:20:26 +0200

    js pour messagerie

--- Views/js/EspaceClient.js
+++ Views/js/EspaceClient.js
@@ -4,9 +4,10 @@ const button2 = document.getElementById("GestCapt");
 console.log(button2);
 const button3 = document.getElementById("GestComptes");
 console.log(button3);
-const button4 = document.getElementById("Parametre");
+const button4 = document.getElementById("MSG");
 console.log(button4);
-
+const button5 = document.getElementById("Parametre");
+console.log(button5);
 var position = 0;
 
 
@@ -19,6 +20,7 @@ button1.addEventListener("click", functionbutton1);
 button2.addEventListener("click", functionbutton2);
 button3.addEventListener("click", functionbutton3);
 button4.addEventListener("click", functionbutton4);
+button5.addEventListener("click", functionbutton5);
 
 function functionbutton1() {
     console.log(this);
@@ -30,6 +32,8 @@ function functionbutton1() {
     button3.style.borderLeft = "none";
     button4.style.background = "#2E4057";
     button4.style.borderLeft = "none";
+    button5.style.background = "#2E4057";
+    button5.style.borderLeft = "none";
 }
 
 function functionbutton2(){
@@ -42,6 +46,8 @@ function functionbutton2(){
     button3.style.borderLeft = "none";
     button4.style.background = "#2E4057";
     button4.style.borderLeft = "none";
+    button5.style.background = "#2E4057";
+    button5.style.borderLeft = "none";
 }
 
 function functionbutton3() {
@@ -54,6 +60,8 @@ function functionbutton3() {
     button2.style.borderLeft = "none";
     button4.style.background = "#2E4057";
     button4.style.borderLeft = "none";
+    button5.style.background = "#2E4057";
+    button5.style.borderLeft = "none";
 }
 
 function functionbutton4(){
@@ -66,6 +74,22 @@ function functionbutton4(){
     button2.style.borderLeft = "none";
     button3.style.background = "#2E4057";
     button3.style.borderLeft = "none";
+    button5.style.background = "#2E4057";
+    button5.style.borderLeft = "none";
+}
+
+function functionbutton5(){
+    console.log(this);
+    button5.style.background = "#0D1F34";
+    button5.style.borderLeft = "3px solid #FD9C17";
+    button1.style.background = "#2E4057";
+    button1.style.borderLeft = "none";
+    button2.style.background = "#2E4057";
+    button2.style.borderLeft = "none";
+    button3.style.background = "#2E4057";
+    button3.style.borderLeft = "none";
+    button4.style.background = "#2E4057";
+    button4.style.borderLeft = "none";
 }
 
 
